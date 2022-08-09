8d131423830f20ad233af1a613ce23964d647430
Tony Greeley <tonygreeley@Tonys-iMac.local>
2022-08-09 18:48:36 -0500

    Solution for Square(n) Sum

---
+++ solutions.js
@@ -0,0 +1,11 @@
+// Square(n) Sum
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+const squareSum = (numbers) => {
+  // initialize a variable that contains the sum
+  let sum = 0;
+  // iterate through the array and for each number square it then add it to the total
+  numbers.forEach((num) => {
+    sum += num * num;
+  });
+  return sum;
+};
