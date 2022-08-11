08b470796c86339af5e8865827c173008d53f1c2
Tony Greeley <tonygreeley@Tonys-iMac.local>
2022-08-10 19:01:36 -0500

    Solution for Is this a triangle?

--- solutions.js
+++ solutions.js
@@ -14,3 +14,14 @@ const squareSum = (numbers) => {
 const greet = () => {
   return 'hello world!';
 };
+
+// Is this a triangle?
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+const isTriangle = (a, b, c) => {
+  // The sum of the sides of any 2 sides must be greater than the length of the third side
+  if (a + b > c && a + c > b && b + c > a) {
+    return true;
+  } else {
+    return false;
+  }
+};
